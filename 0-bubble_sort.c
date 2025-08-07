f7dddec47fe6344265c99e3feb9bc0ba00a2b892
Franck <franck.spadotto@yahoo.com>
2025-08-07 15:08:27 +0200

    Undo changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,21 +10,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp = 0; 
-	size_t i, j;
+	unsigned int long i, j, temp;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
